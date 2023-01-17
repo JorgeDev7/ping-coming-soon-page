fd339677c25d0b3b2e5666cf7099e9b6a9c404d5
JorgeDev7 <jorgehumbertocolindres@gmail.com>
2023-01-17 10:09:30 -0600

    Colores, Colores de fondo y colores de bordes agregados

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,39 @@
 module.exports = {
   content: ["./index.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        // Primary
+        "blue": "hsl(223, 87%, 63%)",
+        // Secondary
+        "pale-blue": "hsl(223, 100%, 88%)",
+        "light-red": "hsl(354, 100%, 66%)",
+        // Neutral
+        "gray": "hsl(0, 0%, 59%)",
+        "very-dark-blue": "hsl(209, 33%, 12%)",
+      },
+      backgroundColor: {
+        // Primary
+        "blue": "hsl(223, 87%, 63%)",
+        // Secondary
+        "pale-blue": "hsl(223, 100%, 88%)",
+        "light-red": "hsl(354, 100%, 66%)",
+        // Neutral
+        "gray": "hsl(0, 0%, 59%)",
+        "very-dark-blue": "hsl(209, 33%, 12%)",
+      },
+      borderColor: {
+        // Primary
+        "blue": "hsl(223, 87%, 63%)",
+        // Secondary
+        "pale-blue": "hsl(223, 100%, 88%)",
+        "light-red": "hsl(354, 100%, 66%)",
+        // Neutral
+        "gray": "hsl(0, 0%, 59%)",
+        "very-dark-blue": "hsl(209, 33%, 12%)",
+      },
+
+    },
   },
   plugins: [],
 }
